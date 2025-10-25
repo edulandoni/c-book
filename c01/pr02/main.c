30deea453d8482ccace79213c87ea87f2f92d78e
Edu Landoni <edulandoni@gmail.com>
2025-10-24 21:58:18 -0300

    pr02.1

--- c01/pr02/main.c
+++ c01/pr02/main.c
@@ -3,7 +3,10 @@
 int main(void){
 
   const float PI = 3.14f;
-  const float r = 10.0f;
+  float r;
+  
+  printf("Radius: ");
+  scanf("%f", &r);
   
   float volume = (4.0f / 3.0f) * PI * ( r * r * r);
   printf("Volume = %.2f\n", volume);
